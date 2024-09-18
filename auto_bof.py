4ad683a2358ca96d088b56ab2bc9951f9d8fb7c9
myrtopar <myrtopar08@gmail.com>
2024-09-19 01:15:05 +0300

    log file bug

--- auto_bof.py
+++ auto_bof.py
@@ -14,6 +14,9 @@ log_file_path = 'strace.log'
 
 
 
+def cleanup():
+    sys.exit(1)
+
 def stack_middle_address(output):
 
     # Find the line containing the word "stack" and extract the address in the middle
@@ -146,11 +149,11 @@ def attach_strace():
 
 def detect_crash(pid: int):
 
-    ppid_str = str(os.getpid())
+    # ppid_str = str(os.getpid())
     pid_str = str(pid)
 
     # pattern to detect the SIGCHLD signal with a SIGSEGV e.g. SIGCHLD {si_signo=SIGCHLD, si_code=CLD_DUMPED, si_pid=15595, si_uid=0, si_status=SIGSEGV, si_utime=0, si_stime=3}
-    sigchld_pattern = re.compile(
+    pattern = re.compile(
         rf"{pid_str}.*SIGCHLD.*si_status=SIGSEGV"
         # rf"{ppid_str}.*SIGCHLD.*si_pid={pid_str}.*si_status=(139|[^,]*SIGSEGV)" => THis line occurs after the exploit process terminates, right now the exploit proc is still running
     )
@@ -159,11 +162,34 @@ def detect_crash(pid: int):
         with open(log_file_path, 'r') as log_file:
             lines = log_file.readlines()
 
-            # Search for a line that matches the crash pattern
             for line in lines:
-                if sigchld_pattern.search(line):
-                    print(line)
-                    print(line)
+                if pattern.search(line):
+                    return True
+
+    except FileNotFoundError:
+        print(f"Log file '{log_file_path}' not found.")
+        cleanup()
+    except Exception as e:
+        print(f"An error occurred while reading the log file: {e}")
+        cleanup()
+
+    return False
+
+def detect_execve():
+
+    pattern = re.compile(
+        r"execve\(\"/bin//sh\".*\) = 0"
+    )
+
+    try:
+        with open(log_file_path, 'r') as log_file:
+            lines = log_file.readlines()
+
+            for line in lines:
+                # print(f"{GREEN}{line}{RESET}")
+                print(line)
+
+                if pattern.search(line):
                     return True
 
     except FileNotFoundError:
@@ -172,7 +198,6 @@ def detect_crash(pid: int):
     except Exception as e:
         print(f"An error occurred while reading the log file: {e}")
         sys.exit(1)
-
 
     return False
 
@@ -215,17 +240,7 @@ def main():
         os.write(master_fd, b'\n')
 
         print(f"\033[94mexploit proc pid: {exploit_proc.pid}\033[0m")
-
-        # while True:
-        #     detect_crash(exploit_proc.pid)
-
-
-
-
-
-        
-
-
+    
         while True:
 
             rlist, _, _ = select.select([master_fd, 0], [], [])
@@ -240,6 +255,9 @@ def main():
                             with open(log_file_path, 'w') as log_file:
                                 log_file.truncate(0)                    #empty the log file from the logs of the previous attempts to minimize the load of the linear search
                             break
+                        elif detect_execve():
+                            print(f"{PINK}Something happened here or what?!{RESET}")
+                            cleanup()
                     else:
                         break
                 except OSError:
@@ -284,45 +302,45 @@ if __name__ == "__main__":
     main()
 
 
-            # while True:
-            # rlist, _, _ = select.select([master_fd, 0], [], [])
+# while True:
+# rlist, _, _ = select.select([master_fd, 0], [], [])
 
-            # if master_fd in rlist:
-            #     try:
-            #         output = os.read(master_fd, 100000)
-            #         if output:
-            #             # os.write(1, output)  # Write the process output to stdout (1)
-            #             if b'Segmentation fault' in output:           #must change, must retrieve the exit code of the process (139)
-            #                 exploit_proc.terminate()
-            #                 break
-            #         else:
-            #             break  # Process has exited
-            #     except OSError:
-            #         break
+# if master_fd in rlist:
+#     try:
+#         output = os.read(master_fd, 100000)
+#         if output:
+#             # os.write(1, output)  # Write the process output to stdout (1)
+#             if b'Segmentation fault' in output:           #must change, must retrieve the exit code of the process (139)
+#                 exploit_proc.terminate()
+#                 break
+#         else:
+#             break  # Process has exited
+#     except OSError:
+#         break
 
-            # if 0 in rlist:
-            #     #exploit took place successfully
-            #     while True:
-            #         os.write(1, b'# ')
-            #         user_input = os.read(0, 5000)
-                    
-            #         if b'exit' in user_input:
-            #                 exploit_proc.terminate()
-            #                 os.close(master_fd)
-            #                 os.close(slave_fd)
-            #                 os.remove("trash")
-            #                 os.remove("vuln_payload")
-            #                 # os.remove("strace.log")
-            #                 sys.exit(1)
-                    
-            #         os.write(master_fd, user_input)  # Forward user input to the exploit shell
-            #         rlist_response, _, _ = select.select([master_fd], [], [], 0.1)
+# if 0 in rlist:
+#     #exploit took place successfully
+#     while True:
+#         os.write(1, b'# ')
+#         user_input = os.read(0, 5000)
+        
+#         if b'exit' in user_input:
+#                 exploit_proc.terminate()
+#                 os.close(master_fd)
+#                 os.close(slave_fd)
+#                 os.remove("trash")
+#                 os.remove("vuln_payload")
+#                 # os.remove("strace.log")
+#                 sys.exit(1)
+        
+#         os.write(master_fd, user_input)  # Forward user input to the exploit shell
+#         rlist_response, _, _ = select.select([master_fd], [], [], 0.1)
 
-            #         while master_fd in rlist_response:
-            #             response = os.read(master_fd, 1024)
-            #             response = response.replace(b'\r', b'')
-            #             if response != user_input:
-            #                 os.write(1, response)
-            #                 break
+#         while master_fd in rlist_response:
+#             response = os.read(master_fd, 1024)
+#             response = response.replace(b'\r', b'')
+#             if response != user_input:
+#                 os.write(1, response)
+#                 break
 
-            #             rlist_response, _, _ = select.select([master_fd], [], [], 0.1)
+#             rlist_response, _, _ = select.select([master_fd], [], [], 0.1)

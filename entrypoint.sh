c3738f34d9439dad7be6080545c7d8d3a1f1be17
Thanassis Avgerinos <thanassis.avgerinos@gmail.com>
2025-03-14 06:02:23 -0400

    Notify user about core parameter setup.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 #disables apport from handling core dumps
-echo "/core_dumps/core.%e.%p" > /proc/sys/kernel/core_pattern
 
+set -euxo pipefail
+
+# TODO: consider alternative for extracting coredumps.
+echo Setting auto autoexploit environment
+echo "/core_dumps/core.%e.%p" > /proc/sys/kernel/core_pattern
 mkdir -p /core_dumps
 chmod 777 /core_dumps
 ulimit -c unlimited
